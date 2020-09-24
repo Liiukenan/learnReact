501fff4e2810c7fd4787eb5330d0cf6d35751e3b
liukenan <liukenan@meetstarinc.com>
2020-09-24 23:57:26 +0800

    路由拓展

---
+++ src/route/Article.js
@@ -0,0 +1,9 @@
+import React from 'react'
+function Article() {
+        return (
+            <div>
+               这是三极路由
+            </div>
+        );
+    }
+export default Article;
--- src/route/Content.js
+++ src/route/Content.js
@@ -3,16 +3,19 @@ import './style.styl'
 import {Route,Link} from 'react-router-dom'
 import Content1 from './Content1'
 import Content2 from './Content2'
+import Article from './Article'
 function Home() {
         return (
             <div className="home">
                 <div className="top">
                     <Link to="/content/content1">跳转到1</Link>
                     <Link to="/content/content2">跳转到2</Link>
+                    <Link to="/article">跳转到3</Link>
                 </div>
                 <div className="contet">
                      <Route path="/Content/Content1"  component={Content1}></Route>
                      <Route path="/Content/Content2"  component={Content2}></Route>
+                     <Route path="/Article"  component={Article}></Route>
                 </div>
             </div>
         );
