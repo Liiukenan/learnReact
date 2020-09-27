136b162179322e65a7127dbf00871d359d78e6a9
liukenan <liukenan@meetstarinc.com>
2020-09-27 15:39:56 +0800

    useReducer的使用

--- src/Hooks/App.js
+++ src/Hooks/App.js
@@ -2,12 +2,12 @@ import React from 'react'
 // import Example1 from './Example1'
 // import Example2 from './Example2'
 // import Example3 from './Example3'
-import Example4 from './Example4'
+import ReducerDemo from './Example4'
 
 function App(props) {
      return (
           <div>
-              <Example4 />
+              <ReducerDemo />
           </div>
       );
 }
--- src/Hooks/Example4.js
+++ src/Hooks/Example4.js
@@ -1,1 +1,19 @@
-// const [state, dispatch] = useReducer(reducer, initialState, init)
+import React, { useReducer } from 'react';
+function ReducerDemo(){
+    const [count,dispatch] = useReducer((state,action)=>{
+        switch(action) {
+            case 'add': return state+1;
+            case 'sub':return state-1;
+            default: return state;
+        }
+    },0)
+    // 0代表默认值
+    return(
+        <div>
+            <h2>分数:{count}</h2>
+            <button onClick={()=>{dispatch('add')}}>加号</button>
+            <button onClick={()=>{dispatch('sub')}}>减号</button>
+        </div>
+    )
+}
+export default ReducerDemo;
