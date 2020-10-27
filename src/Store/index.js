71ea21363a3675db935cf5fbe0592a9569955e10
liukenan <liukenan@meetstarinc.com>
2020-10-27 11:10:09 +0800

    更新后保存本地数据

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import { createModel } from "hox";
-import { useSessionStorageState } from 'ahooks';
+import { useSessionStorageState, useUpdateEffect} from 'ahooks';
 function useCounter(initialValue) {
 //   const [count, setCount] = useState(0);
   const [message, setMessage] = useSessionStorageState('user-message', '5');
   const [count,setCount]=useState(initialValue ?? JSON.parse(message));
+  useUpdateEffect(() => {
+    // 每次更新都执行
+    setMessage(count);
+    return () => {
+      // do something
+      // 第一次不执行，随后每次组件更新后执行
+      
+    };
+  }, [count]);
   const decrement = () =>{
       setCount(count - 1);
-      setMessage(count-1);
+      
   } 
   const increment = () =>{
      setCount(count + 1); 
-     setMessage(count+1);
   }
   return {
     count,
